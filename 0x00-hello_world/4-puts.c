f08ae03050e586393f439674e37fde8307667638
pweetyaries <bakarebusayo580@gmail.com>
2022-09-10 10:53:33 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - main block
+ * main - main function block
  * Return: 0
  */
 int main(void)
 {
-        puts("/"programming is like building a multilingual puzzle");
+        puts("\"programming is like building a multilingual puzzle");
         return (0);
 }

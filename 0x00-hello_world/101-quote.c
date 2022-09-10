f3108783dcc7f60f1f64ce4acd9837b7fffea51e
pweetyaries <bakarebusayo580@gmail.com>
2022-09-10 16:02:38 -0700

    right quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
-* main - print function
- * Return: 1 if success
+*
+* main-Entry point
+* Return: Always 1 (success)
 */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }

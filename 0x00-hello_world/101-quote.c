ecdd3a30c0b232a62dc1ec6ba7c80059b9722a67
pweetyaries <bakarebusayo580@gmail.com>
2022-09-10 15:33:22 -0700

    intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - print function
+ * Return: 1 if success
+*/
+int main(void)
+{
+	("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
+}

bf6499736546a81efc9d43f3337c4b8597d30427
pweetyaries <bakarebusayo580@gmail.com>
2022-09-10 12:59:16 -0700

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /**
  * main - prints like "Programming is like building a multilingual puzzle
  * Return: 0 if success.
- */
+*/
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+            return (0);
 }

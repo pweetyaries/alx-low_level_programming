685e9d138be75fe673ce76f6b9f7acfc31de2768
pweetyaries <bakarebusayo580@gmail.com>
2022-09-10 11:13:57 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 
 /**
  * main - main function block
+ * print - Programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
 
-/**
- * main - print main string
- *
- * description: using the main string
- * Return: 0
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}

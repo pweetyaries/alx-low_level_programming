84e98aa789dbc075ddcce9117320de11ce92c077
pweetyaries <bakarebusayo580@gmail.com>
2022-09-12 11:05:04 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the possible combination of single-digit function
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int d;
+	
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(d);
+	}
+	if (d != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

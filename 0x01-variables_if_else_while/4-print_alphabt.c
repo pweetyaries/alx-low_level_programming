a57fddd9d15db4dc44e4498ceb134cf977533c4d
pweetyaries <bakarebusayo580@gmail.com>
2022-09-12 08:53:21 -0700

    alphat

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the function
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char alx[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alx[i]);
+	}
+	putchar('\n');
+	return (0);
+}

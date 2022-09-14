8617d9842784279ce984b7aa9528731e412c9c4c
pweetyaries <bakarebusayo580@gmail.com>
2022-09-14 11:51:46 -0700

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * islower - Entry point
+ * islower - Entry point for lowercase
  * lowercase character. Another cases, shows 0
  * @c: The character in ASCII code
  * Return: 1 for lowercase character. 0 for the rest.
  */
-
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')

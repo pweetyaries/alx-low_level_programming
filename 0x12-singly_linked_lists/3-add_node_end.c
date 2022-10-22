6242e1d26816b6fa76fafe29d3b281a8f261d0c2
pweetyaries <bakarebusayo580@gmail.com>
2022-10-21 21:00:27 -0700

    no trailing space

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,14 +18,11 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	
-	for (nchar=0; str[nchar]; nchar++)
+	for (nchar = 0; str[nchar]; nchar++)
 		;
-	
 	new->len = nchar;
 	new->next = NULL;
-	temp = *head;
-	
+	temp = *head;	
 	if (temp == NULL)
 		*head = new;
 	else

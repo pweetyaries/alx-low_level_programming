a09979a0523641d3e2d9dbfa264a98946cb67377
pweetyaries <bakarebusayo580@gmail.com>
2023-03-14 23:24:29 -0700

    diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,16 +3,15 @@
 
 /**
  * print_diagsums - prints the sum of the two
- * diagonals of a square matrix of integers
+ *    diagonals of a square matrix of integers.
  * @a: input pointer.
  * @size: size of the matrix
- * Return: no return.
  */
 
 void print_diagsums(int *a, int size)
 {
 	int i, sum1 = 0, sum2 = 0;
-
+	
 	for (i = 0; i < (size * size); i++)
 	{
 		if (i % (size + 1) == 0)sum1 += *(a + i);
